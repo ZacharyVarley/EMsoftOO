628b9f6889033eaeac09c37665211955d3f87818
ZacharyVarley <33402037+ZacharyVarley@users.noreply.github.com>
2024-05-21 21:28:53 -0400

    Update build_ubuntu.sh

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Clone EMsoft and set up SDK Debug/Release
+# Clone EMsoft and set up SDK
+cd $HOME
 git clone --branch developOO https://github.com/EMsoft-org/EMsoftSuperbuild.git
 mv EMsoftSuperbuild EMsoftOOSuperbuild
 cd EMsoftOOSuperbuild && mkdir Release
 
 # Build EMsoftSuperbuild
 cd $HOME/EMsoftOOSuperbuild/Release/
-cmake -DEMsoftOO_SDK=/opt/EMsoftOO_SDK -DCMAKE_BUILD_TYPE=Release ../ -G Ninja && ninja
+cmake -DEMsoftOO_SDK=$HOME/EMsoftOO_SDK -DCMAKE_BUILD_TYPE=Release ../ -G Ninja && ninja
 
 # Clone EMsoftOO and EMsoftData
 cd $HOME
@@ -16,7 +17,7 @@ mkdir EMsoftOOBuild
 
 # Build EMsoftOO
 cd $HOME/EMsoftOOBuild/ && mkdir Release && cd Release
-cmake -DCMAKE_BUILD_TYPE=Debug -DEMsoftOO_SDK=/opt/EMsoftOO_SDK -DBUILD_SHARED_LIBS=OFF ../../EMsoftOO -G Ninja
+cmake -DCMAKE_BUILD_TYPE=Debug -DEMsoftOO_SDK=$HOME/EMsoftOO_SDK -DBUILD_SHARED_LIBS=OFF ../../EMsoftOO -G Ninja
 ninja
 
 # Package EMsoftOO
