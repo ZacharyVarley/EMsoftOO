af162df85bccb27a3edcb483dd5bd434ce0bebf7
ZacharyVarley <33402037+ZacharyVarley@users.noreply.github.com>
2024-08-19 14:23:45 -0400

    Update build_macos.sh

--- build_macos.sh
+++ build_macos.sh
@@ -10,17 +10,17 @@ mv EMsoftSuperbuild EMsoftOOSuperbuild
 cd EMsoftOOSuperbuild && mkdir Release && cd Release
 
 # Build EMsoftSuperbuild
-cmake -DEMsoftOO_SDK=/opt/EMsoftOO_SDK -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=$GFORTRAN_PATH ../ -G Ninja && ninja
+sudo cmake -DEMsoftOO_SDK=/opt/EMsoftOO_SDK -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=$GFORTRAN_PATH ../ -G Ninja && sudo ninja
 
 # Clone EMsoftOO and EMsoftData
 cd ../..
 git clone https://github.com/EMsoft-org/EMsoftData.git
 git clone https://github.com/ZacharyVarley/EMsoftOO.git
-mkdir -p EMsoftOOBuild
+sudo mkdir -p EMsoftOOBuild
 
 # Build EMsoftOO
 cd EMsoftOOBuild && mkdir Release && cd Release
-cmake -DCMAKE_BUILD_TYPE=Release -DEMsoftOO_SDK=/opt/EMsoftOO_SDK -DBUILD_SHARED_LIBS=OFF -DCMAKE_Fortran_COMPILER=$GFORTRAN_PATH ../../EMsoftOO -G Ninja && ninja
+sudo cmake -DCMAKE_BUILD_TYPE=Release -DEMsoftOO_SDK=/opt/EMsoftOO_SDK -DBUILD_SHARED_LIBS=OFF -DCMAKE_Fortran_COMPILER=$GFORTRAN_PATH ../../EMsoftOO -G Ninja && sudo ninja
 
 # Package Release EMsoftOO
 cpack -G DragNDrop
