296624066f17695e5f8b04ac88457996419949b7
ZacharyVarley <33402037+ZacharyVarley@users.noreply.github.com>
2024-08-19 15:31:08 -0400

    Update build_macos.sh

--- build_macos.sh
+++ build_macos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Find gfortran path
-$FC=$(brew --prefix gcc)/bin/gfortran
-$FC_PATH=$(brew --prefix gcc)/bin/gfortran
+export $FC=$(brew --prefix gcc)/bin/gfortran
+export $FC_PATH=$(brew --prefix gcc)/bin/gfortran
 
 # Clone EMsoft and set up SDK Release
 git clone --branch developOO https://github.com/EMsoft-org/EMsoftSuperbuild.git
